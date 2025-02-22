3d5990f30a612f3bed9f202cb623fd4d529540da
khandelwala812 <avikwal123@gmail.com>
2025-02-22 14:34:29 -0500

    initial ui commit

---
+++ ui/src/app/(client)/dashboard/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import InterviewCard from "@/components/dashboard/interview/interviewCard";
+import CreateInterviewCard from "@/components/dashboard/interview/createInterviewCard";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { InterviewService } from "@/services/interviews.service";
+import { ClientService } from "@/services/clients.service";
+import { ResponseService } from "@/services/responses.service";
+import { useInterviews } from "@/contexts/interviews.context";
+import Modal from "@/components/dashboard/Modal";
+import { Gem, Plus } from "lucide-react";
+import Image from "next/image";
+
+function Interviews() {
+  const { interviews, interviewsLoading } = useInterviews();
+  // const [loading, setLoading] = useState<boolean>(true);
+
+  function InterviewsLoader() {
+    return (
+      <>
+        <div className="flex flex-row">
+          <div className="h-60 w-56 ml-1 mr-3 mt-3 flex-none animate-pulse rounded-xl bg-gray-300" />
+          <div className="h-60 w-56 ml-1 mr-3  mt-3 flex-none animate-pulse rounded-xl bg-gray-300" />
+          <div className="h-60 w-56 ml-1 mr-3 mt-3 flex-none animate-pulse rounded-xl bg-gray-300" />
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <main className="p-8 pt-0 ml-12 mr-auto rounded-md">
+      <div className="flex flex-col items-left">
+        <h2 className="mr-2 text-2xl font-semibold tracking-tight mt-8">
+          My Interviews
+        </h2>
+        <h3 className=" text-sm tracking-tight text-gray-600 font-medium ">
+          Practice interviewing now!
+        </h3>
+        <div className="relative flex items-center mt-1 flex-wrap">
+          <CreateInterviewCard />
+          {interviewsLoading ? (
+            <InterviewsLoader />
+          ) : (
+            <>
+              {interviews.map((item, index) => (
+                <InterviewCard
+                  key={index}
+                  id={String(index)}
+                  name={item.name}
+                />
+              ))}
+            </>
+          )}
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Interviews;
---
+++ ui/src/components/dashboard/interview/interviewCard.tsx
@@ -0,0 +1,103 @@
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+import { Copy } from "lucide-react";
+import { CopyCheck } from "lucide-react";
+import axios from "axios";
+import MiniLoader from "@/components/loaders/mini-loader/miniLoader";
+import { InterviewerService } from "@/services/interviewers.service";
+
+interface Props {
+  name: string | null;
+  id: string;
+  // url: string;
+  // readableSlug: string;
+}
+
+const base_url = process.env.NEXT_PUBLIC_LIVE_URL;
+
+function InterviewCard({ name, id }: Props) {
+  const [copied, setCopied] = useState(false);
+  const [responseCount, setResponseCount] = useState<number | null>(null);
+  const [isFetching, setIsFetching] = useState(false);
+
+  // const copyToClipboard = () => {
+  //   navigator.clipboard
+  //     .writeText(
+  //       readableSlug ? `${base_url}/call/${readableSlug}` : (url as string),
+  //     )
+  //     .then(
+  //       () => {
+  //         setCopied(true);
+  //         toast.success(
+  //           "The link to your interview has been copied to your clipboard.",
+  //           {
+  //             position: "bottom-right",
+  //             duration: 3000,
+  //           },
+  //         );
+  //         setTimeout(() => {
+  //           setCopied(false);
+  //         }, 2000);
+  //       },
+  //       (err) => {
+  //         console.log("failed to copy", err.mesage);
+  //       },
+  //     );
+  // };
+
+  return (
+    <a
+      href={`/interviews/${id}`}
+      style={{
+        pointerEvents: isFetching ? "none" : "auto",
+        cursor: isFetching ? "default" : "pointer",
+      }}
+    >
+      <Card className="relative p-0 mt-4 inline-block cursor-pointer h-60 w-56 ml-1 mr-3 rounded-xl shrink-0 overflow-hidden shadow-md">
+        <CardContent className={`p-0 ${isFetching ? "opacity-60" : ""}`}>
+          <div className="w-full h-40 overflow-hidden bg-indigo-600 flex items-center text-center">
+            <CardTitle className="w-full mt-3 mx-2 text-white text-lg">
+              {name}
+              {isFetching && (
+                <div className="z-100 mt-[-5px]">
+                  <MiniLoader />
+                </div>
+              )}
+            </CardTitle>
+          </div>
+          <div className="flex flex-row items-center mx-4 ">
+            <div className="w-full overflow-hidden">
+              <Image
+                src={'/interviewers/Lisa.png'}
+                alt="Picture of the interviewer"
+                width={70}
+                height={70}
+                className="object-cover object-center"
+              />
+            </div>
+          </div>
+          <div className="absolute top-2 right-2">
+            <Button
+              className={`text-xs text-indigo-600 px-1 h-6  ${
+                copied ? "bg-indigo-300 text-white" : ""
+              }`}
+              variant={"secondary"}
+              onClick={(event) => {
+                event.stopPropagation();
+                event.preventDefault();
+                // copyToClipboard();
+              }}
+            >
+              {copied ? <CopyCheck size={16} /> : <Copy size={16} />}
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </a>
+  );
+}
+
+export default InterviewCard;
