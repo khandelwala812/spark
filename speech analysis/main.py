adf89767e07f29dfd7e745b81c3d63af5b7b49cd
Jack <jackji.2004@gmail.com>
2025-02-22 19:34:52 -0500

    main call file for speech analysis

--- speech analysis/main.py
+++ speech analysis/main.py
@@ -1,6 +1,27 @@
-import myprosody as mysp
+import sys
+import flow_analysis as flow
 
-p = "Recording"
-c = r"C:\Voice\myprosody"
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python analyze_speech.py <audio.wav>")
+        sys.exit(1)
 
-mysp.mysptotal(p, c)
+    flowdir = r"C:\Voice\flow_analysis"
+    filename = sys.argv[1]
+
+    try:
+        print(flowdir+"/dataset/wav_files/"+filename)
+        with open(flowdir+"/dataset/wav_files/"+filename+".wav", 'r') as file:
+            print(f"Found file '{filename}'")
+            print("Number of Pauses:", flow.getPauses(filename, flowdir))
+            print("Rate of Speech:", flow.getRateOfSpeech(filename, flowdir))
+            print("Rate of Articulation:", flow.getRateOfArticulation(filename, flowdir))
+            print("Pronunciation Correctness Percentage:", flow.getPronunciationPercentageCorrect(filename, flowdir))
+
+
+
+
+    except FileNotFoundError:
+        print(f"Error: Could not find file '{filename}'")
+    except Exception as e:
+        print(f"An error occurred: {e}")
