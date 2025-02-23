e32ad62ba973be1853dacb7769c55371105d9bcb
Jack <jackji.2004@gmail.com>
2025-02-23 02:19:03 -0500

    ui modifications

--- ui/src/components/navbar.tsx
+++ ui/src/components/navbar.tsx
@@ -9,11 +9,9 @@ function Navbar() {
         <div className="flex flex-row gap-3 justify-center">
           <Link href={"/dashboard"} className="flex items-center gap-2">
             <p className="px-2 py-1 text-2xl font-bold text-black">
-              Folo<span className="text-indigo-600">Up</span>{" "}
-              <span className="text-[8px]">Beta</span>
+              <span className="text-blue-500">Spark</span>{" "}
             </p>
           </Link>
-          <p className="my-auto text-xl">/</p>
           <div className="my-auto">
             {/* <OrganizationSwitcher
               afterCreateOrganizationUrl="/dashboard"
